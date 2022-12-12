18ec3ec213cc22b4681ae26dbc022da43b7680c6
Raya Sabari <rayasabari@Rayas-MacBook-Air.local>
2022-12-12 17:41:01 +0700

    Update : Add navigation link and revamp brand logo

--- components/elements/Icons/Enterwind.js
+++ components/elements/Icons/Enterwind.js
@@ -1,12 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function Logo() {
   return (
     <div className="flex gap-2">
-      <div className="flex items-center gap-1">
-        <div className="font-semibold tracking-wider">Enterwind</div>
-        <div className="flex items-center px-1 text-xs font-medium tracking-widest border rounded border-slate-600">UI</div>
-      </div>
+      <Link className="flex items-center gap-1 text-2xl" href={'/'}>
+        <div className="font-medium">enterwind</div>
+        <div className="flex items-center px-1 text-sm font-semibold tracking-widest border rounded border-slate-600">UI</div>
+      </Link>
     </div>
   );
 }
--- components/groups/Navbar/Navbar.js
+++ components/groups/Navbar/Navbar.js
@@ -1,12 +1,20 @@
+import Link from 'next/link';
 import React from 'react';
 import { EnterWind as Logo, Github } from '../../elements/Icons';
 
-function Navbar(){
+function Navbar() {
   return (
-    <header className="fixed top-0 left-0 right-0 flex justify-center bg-white shadow">
-      <div className="container flex justify-between px-3 py-3 bg-white lg:py-5">
-        <Logo />
-        <nav>
+    <header className="fixed top-0 left-0 right-0 flex justify-center shadow">
+      <div className="container flex justify-between px-3 py-3 lg:py-5">
+        <div className="flex items-center gap-4 lg:gap-10">
+          <Logo />
+          <nav className="items-center hidden gap-4 text-sm md:flex">
+            <Link className="transition-all duration-300 hover:font-medium" href={'/components'}>Components</Link>
+            <Link className="transition-all duration-300 hover:font-medium" href={'/documentation'}>Docs</Link>
+            <Link className="transition-all duration-300 hover:font-medium" href={'/blog'}>Blog</Link>
+          </nav>
+        </div>
+        <nav className="flex items-center">
           <a href="https://github.com/rayasabari/enterwindui">
             <Github size="w-5 h-5" />
           </a>
