d703574a788b46813155a05a339517a472a360e9
Raya Sabari <hi@rayasabari.com>
2022-12-13 22:52:01 +0700

    Fix Github icon color dark-mode, update Github url

--- components/groups/Navbar/Navbar.js
+++ components/groups/Navbar/Navbar.js
@@ -24,25 +24,25 @@ function Navbar() {
 
   useEffect(() => {
     checkTheme();
-  });
+  }, []);
 
   return (
     <header className="fixed top-0 left-0 right-0 z-10 flex justify-center transition bg-white shadow dark:bg-slate-900">
-      <div className="container flex items-center justify-between px-3 py-3 lg:py-5">
+      <div className="container flex items-center justify-between px-4 py-3 lg:py-5">
         <div className="flex items-center gap-4 lg:gap-10">
           <Logo />
+        </div>
+        <nav className="flex items-center gap-3 md:gap-4">
           <nav className="items-center hidden gap-4 text-sm md:flex">
             <Link className="hover:font-medium" href={'/components'}>Components</Link>
             <Link className="hover:font-medium" href={'/documentation'}>Docs</Link>
             <Link className="hover:font-medium" href={'/blog'}>Blog</Link>
           </nav>
-        </div>
-        <nav className="flex items-center gap-3 md:gap-5">
-          <button className="w-6 h-6" onClick={toggleDarkMode} type="button">
+          <button className="w-5 h-5" onClick={toggleDarkMode} type="button">
             {dark ? <SunIcon className="text-yellow-500" /> : <MoonIcon />}
           </button>
-          <a href="https://github.com/rayasabari/enterwindui">
-            <Github size="w-5 h-5" />
+          <a href="https://github.com/Emesge/enterwindui">
+            <Github size="w-5 h-5 dark:text-slate-50" />
           </a>
         </nav>
       </div>
